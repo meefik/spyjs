74bc97ae10f53ca36d82157a3b724010f38af36d
Anton Skshidlevsky <meefik@gmail.com>
2013-11-10 23:07:23 +0400

    Added report API.

--- routes/api.js
+++ routes/api.js
@@ -43,3 +43,81 @@ exports.add = function(req, res){
     }
 
 }
+
+exports.list = function(req, res) {
+    res.set('Content-Type', 'text/plain');
+    switch (req.query.table) {
+        case 'pages':
+            pool.getConnection(function(err, connection) {
+                var sql = "SELECT * FROM pages";
+                var params = [];
+                if (req.query.userid) {
+                    var sql = "SELECT * FROM pages WHERE userid=?";
+                    var params = [req.query.userid];
+                }
+                connection.query(sql, params, function (err, results) {
+                    if (!err) {
+                        var response = '"pageid";"timestamp";"userid";"uri";"width";"height";"title";"useragent"\n';
+                        for (var i = 0; i < results.length; i++) {
+                            response += '"'+results[i].pageid+'";"'+results[i].timestamp+'";"'+results[i].userid+'";"'+
+                                results[i].uri+'";"'+results[i].width+'";"'+results[i].height+'";"'+results[i].title+'";"'+results[i].useragent+'"\n';
+                        }
+                        res.send(200, response);
+                    } else {
+                        res.send(406, "Not Acceptable");
+                        console.log(err);
+                    }
+                    connection.release();
+                });
+            });
+            break;
+        case 'keyboard':
+            if (!req.query.pageid) {
+                return res.send(400, "Bad Request");
+            }
+            pool.getConnection(function(err, connection) {
+                var sql = "SELECT * FROM keyboard WHERE pageid=?";
+                var params = [req.query.pageid];
+                connection.query(sql, params, function (err, results) {
+                    if (!err) {
+                        var response = '"pageid";"time";"keycode";"delta"\n';
+                        for (var i = 0; i < results.length; i++) {
+                            response += '"'+results[i].pageid+'";"'+results[i].time+'";"'+results[i].keycode+'";"'+
+                                results[i].delta+'"\n';
+                        }
+                        res.send(200, response);
+                    } else {
+                        res.send(406, "Not Acceptable");
+                        console.log(err);
+                    }
+                    connection.release();
+                });
+            });
+            break;
+        case 'mouse':
+            if (!req.query.pageid) {
+                return res.send(400, "Bad Request");
+            }
+            pool.getConnection(function(err, connection) {
+                var sql = "SELECT * FROM mouse WHERE pageid=?";
+                var params = [req.query.pageid];
+                connection.query(sql, params, function (err, results) {
+                    if (!err) {
+                        var response = '"pageid";"time";"keycode";"delta";"x";"y";"tag"\n';
+                        for (var i = 0; i < results.length; i++) {
+                            response += '"'+results[i].pageid+'";"'+results[i].time+'";"'+results[i].keycode+'";"'+
+                                results[i].delta+'";"'+results[i].x+'";"'+results[i].y+'";"'+results[i].tag+'"\n';
+                        }
+                        res.send(200, response);
+                    } else {
+                        res.send(406, "Not Acceptable");
+                        console.log(err);
+                    }
+                    connection.release();
+                });
+            });
+            break;
+        default:
+            res.send(400, "Bad Request");
+    }
+}
